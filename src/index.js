7ee38d37ca44355c1f9831ccfe3d87bcf5aec013
André Leite <andreleite002@gmail.com>
2023-01-09 13:35:12 -0300

    feat: complete middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,85 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  
+  if(!user) {
+      return response.status(404).json({
+          error: "Usuário não encontrado!"
+      });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length >= 10) {
+      return response.status(403).json({
+          error: "Ops! Você passou o seu limite de todos. Máx: 10"
+      });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(!verifyUUUID(id)){
+    return response.status(400).json({
+      error: "ID todo inválido"
+  });
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({
+        error: "Usuário não encontrado!"
+    });
+  }
+
+  todoIndex =  user.todos.findIndex(todo => todo.id === id);
+
+  if(todoIndex < 0) {
+    return response.status(404).json({
+        error: "TODO não encontrado!"
+    });
+  }
+
+  request.user = user;
+  request.todo =  user.todos[todoIndex];
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+   const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  
+  if(!user) {
+      return response.status(404).json({
+          error: "Usuário não encontrado!"
+      });
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+function verifyUUUID(id){
+  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+  return regexExp.test(id);
 }
 
 app.post('/users', (request, response) => {
